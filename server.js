ab059db527b98d8b3f5cfc04d0f97c038d6f61a3
danny-817 <d_halsall@hotmail.com>
2023-10-26 17:18:43 +0100

    test written for bad path

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ mongoose.connect(process.env.TEST_DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 
-db.once("open", () => console.log("connected to database"));
+//db.once("open", () => console.log("connected to database"));
 const ENV = process.env.NODE_ENV;
-//console.log(ENV);
+
 app.use(express.json());
 
 const paintSchemeRouter = require("./routes/paintSchemeRoutes");
 
 app.use("/api/paintschemes", paintSchemeRouter);
+
+app.use((req, res) => {
+  res.status(404).send({ msg: "url not found" });
+});
 
 //app.listen(9090, () => console.log("server started"));
 module.exports = app;
