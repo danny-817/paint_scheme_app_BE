767c343ccd87a4e0d4a2e47ef833e2556b25f355
Daniel Halsall <dhalsall@Daniels-MacBook-Pro.local>
2024-04-02 17:03:40 +0100

    tests added for invalid username and 0 length scheme name

--- models/createNewScheme.model.js
+++ models/createNewScheme.model.js
@@ -1,7 +1,23 @@
 const paintScheme = require("../schema/paintSchemeModel");
+const userProfile = require("../schema/userModel");
 
 async function createNewScheme(newScheme) {
 	const schemeToAdd = new paintScheme(newScheme);
+	const checkUser = await userProfile.exists({
+		username: schemeToAdd.username,
+	});
+	if (!checkUser) {
+		return Promise.reject({
+			status: 400,
+			msg: "Bad request",
+		});
+	}
+	if (schemeToAdd.scheme_name.length === 0) {
+		return Promise.reject({
+			status: 400,
+			msg: "Please enter a name for your paint scheme",
+		});
+	}
 	const checkDoc = await paintScheme.exists({
 		username: schemeToAdd.username,
 		scheme_name: schemeToAdd.scheme_name,
